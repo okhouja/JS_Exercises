e12e8009fabc7f6fe669a401a384a04bffbb0bb4
Omar <omar0940@gmail.com>
2021-02-18 09:18:07 +0100

    method_1 Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("======1st_Assignment======Word Converter====");
 
-/* /*##### 3. Word Converter
+/* Word Converter
 
 - Given an array of words, create a function named "_wordConverter_" which uses an array method to add _"er"_ to the end of each string in the array. Look at the expected output below. **Important**: Do **not** use a loop to complete this task. Print your solution to the console.
 
@@ -23,23 +23,6 @@ function wordConverter(arr) {
 }
 
 console.log(wordConverter(str));
-// 2nd Way
-const wordConv = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] += "er";
-  }
-  return arr;
-};
-console.log(wordConv(str));
-
-// 3rd Way
-const wordCon = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i].concat("er");
-  }
-  return arr;
-};
-console.log(wordCon(str));
 
 console.log("======2nd_Assignment======toCamelCase====");
 
@@ -52,26 +35,6 @@ const toCamelCase = (str) => {
   return result.join("");
 };
 console.log(toCamelCase("cool_fun_things"));
-// 2nd way
-const camelCase = (str) => {
-  let words = str.split("_");
-  console.log(words);
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
-  }
-
-  return words.join(" ");
-};
-console.log(camelCase("Coool_fUn_tHinG"));
-
-//3rd Way
-const Camel = (str) => {
-  const words = str.toLowerCase().split(" ");
-  console.log(words);
-  let result = words.map((item) => item[0].toUpperCase() + item.slice(1));
-  return result.join(" ");
-};
-console.log(Camel("iNterNet is an awEsome resource"));
 
 console.log("======3rd_Assignment======hackerSpeak====");
 // 1st way *
@@ -98,60 +61,3 @@ const hackSpeak = (str) => {
     .join("");
 };
 console.log(hackSpeak("I like javaScript"));
-//2nd way
-const hackerSpeak = (str) => {
-  const lowerStr = str.toLowerCase();
-  const letters = lowerStr.split("");
-  const hackerSpeak = [];
-  for (let i = 0; i < letters.length; i++) {
-    switch (letters[i]) {
-      case "a":
-        letters[i] = "4";
-        break;
-      case "e":
-        letters[i] = "3";
-        break;
-      case "i":
-        letters[i] = "1";
-        break;
-      case "o":
-        letters[i] = "0";
-        break;
-      case "s":
-        letters[i] = "5";
-        break;
-      default:
-        letters[i];
-    }
-    hackerSpeak.push(letters[i]);
-  }
-  return hackerSpeak.join("");
-};
-
-console.log(hackerSpeak("become a coder"));
-
-// 3rd Way
-const hackSpeaker = (str) => {
-  const change = { a: 4, b: 3, i: 1, o: 0, s: 5 };
-  const speak = (el) => (change.hasOwnProperty(el) ? change[el] : el);
-
-  return str.split("").map(speak).join("");
-};
-
-console.log(hackSpeaker("i become a super coder"));
-console.log(hackSpeaker("hackerSpeak javascript"));
-
-// 4th Way
-
-const hs3 = (orig) => {
-  replacements = { a: "4", e: "3", i: "1", o: "0" };
-  return Object.entries(replacements).reduce(
-    (acc, el) => acc.replace(el[0], el[1]),
-    orig
-  );
-};
-
-console.log(hs3("life"));
-console.log(hs3("air"));
-console.log(hs3("none"));
-console.log(hs3("javascript"));
