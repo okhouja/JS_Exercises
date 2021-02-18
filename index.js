87f5e2840c53789176f70728361a867af865778c
Omar <omar0940@gmail.com>
2021-02-18 09:55:34 +0100

    main update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("======1st_Assignment======Word Converter====");
 
-/* Word Converter
+/*  Word Converter
 
 - Given an array of words, create a function named "_wordConverter_" which uses an array method to add _"er"_ to the end of each string in the array. Look at the expected output below. **Important**: Do **not** use a loop to complete this task. Print your solution to the console.
 
@@ -15,9 +15,21 @@ javascript
 ["smarter", "kinder", "sweeter", "smaller", "clearer"];
 */
 // 1st Way
-// in method_1
+let str = ["smart", "kind", "sweet", "small", "clear"];
+
+function wordConverter(arr) {
+  return arr.map((word) => (word += "er"));
+}
+
+console.log(wordConverter(str));
 // 2nd Way
-// in method_2
+const wordConv = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += "er";
+  }
+  return arr;
+};
+console.log(wordConv(str));
 
 // 3rd Way
 const wordCon = (arr) => {
@@ -31,12 +43,27 @@ console.log(wordCon(str));
 console.log("======2nd_Assignment======toCamelCase====");
 
 // 1st Way
-// in method_1
+const toCamelCase = (str) => {
+  let strToArr = str.toLowerCase().split("_");
+  let result = strToArr.map((item, i) =>
+    1 > i ? item : item[0].toUpperCase() + item.slice(1)
+  );
+  return result.join("");
+};
+console.log(toCamelCase("cool_fun_things"));
 // 2nd way
-// in method_2
+const camelCase = (str) => {
+  let words = str.split("_");
+  console.log(words);
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
+  }
 
-// 3rd Way
+  return words.join(" ");
+};
+console.log(camelCase("Coool_fUn_tHinG"));
 
+//3rd Way
 const Camel = (str) => {
   const words = str.toLowerCase().split(" ");
   console.log(words);
@@ -47,11 +74,64 @@ console.log(Camel("iNterNet is an awEsome resource"));
 
 console.log("======3rd_Assignment======hackerSpeak====");
 // 1st way *
-// in method_1
+const hackerSpeak = (str) => {
+  return str
+    .toLowerCase()
+    .split("")
+    .map((chr) => {
+      switch (chr) {
+        case "a":
+          return "4";
+        case "e":
+          return "3";
+        case "i":
+          return "1";
+        case "o":
+          return "0";
+        case "s":
+          return "5";
+        default:
+          return chr;
+      }
+    })
+    .join("");
+};
+console.log(hackerSpeak("I like javaScript"));
 // 2nd way
-// in method_2
+
+const hackerSpeak2 = (str) => {
+  const lowerStr = str.toLowerCase();
+  const letters = lowerStr.split("");
+  const hackerSpeak = [];
+  for (let i = 0; i < letters.length; i++) {
+    switch (letters[i]) {
+      case "a":
+        letters[i] = "4";
+        break;
+      case "e":
+        letters[i] = "3";
+        break;
+      case "i":
+        letters[i] = "1";
+        break;
+      case "o":
+        letters[i] = "0";
+        break;
+      case "s":
+        letters[i] = "5";
+        break;
+      default:
+        letters[i];
+    }
+    hackerSpeak.push(letters[i]);
+  }
+  return hackerSpeak.join("");
+};
+
+console.log(hackerSpeak("become a coder"));
 
 // 3rd Way
+
 const hackerSpeaker3 = (str) => {
   const change = { a: 4, b: 3, i: 1, o: 0, s: 5 };
   const speak = (el) => (change.hasOwnProperty(el) ? change[el] : el);
@@ -64,8 +144,34 @@ console.log(hackerSpeaker3("hackerSpeak javascript"));
 console.log(hackerSpeaker3("javascript Awsome Jaaaaa its works"));
 
 // 4th Way
-// in main branch
+
+const hackerSpeak4 = (orig) => {
+  replacements = { a: "4", e: "3", i: "1", o: "0" };
+  return Object.entries(replacements).reduce(
+    (acc, el) => acc.replace(el[0], el[1]),
+    orig
+  );
+};
+
+console.log(hackerSpeak4("life"));
+console.log(hackerSpeak4("air"));
+console.log(hackerSpeak4("none"));
+console.log(hackerSpeak4("javascript Awsome Jaaaaa its not working here"));
 
 // 5th Way
 
-// in main branch
+const dictionary = {
+  a: 4,
+  e: 3,
+  i: 1,
+  o: 0,
+  s: 5,
+};
+
+function hackerSpeak5(str) {
+  return str.replace(/[aeios]/gi, (m) => dictionary[m]);
+  // it will return new, modified string
+}
+
+console.log(hackerSpeak5("become a coder"));
+console.log(hackerSpeak5("javascript Awsome Jaaaaa its works"));
